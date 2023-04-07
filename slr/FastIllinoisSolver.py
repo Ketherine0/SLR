e894de71ea28ceb36bea40ef40c3b69075b3c420
ashlynzzzz <kxinzhao@umich.edu>
2023-04-07 19:29:58 -0400

    Update FastIllinoisSolver.py

--- slr/FastIllinoisSolver.py
+++ slr/FastIllinoisSolver.py
@@ -1,7 +1,7 @@
 import numpy as np
 from shrink import shrink
 
-def FastIllinoisSolver(DtD, Dtb, x, tauInv, betaTauInv, lasso_max_iter):
+def FastIllinoisSolver(DtD, Dtb, x, D, f, tauInv, betaTauInv, lasso_max_iter):
     '''
     Solve lasso problem: x_{k+1}= argmin x : (2/beta)||x||_1 + ||b-Dx||_2^2
 
@@ -9,6 +9,7 @@ def FastIllinoisSolver(DtD, Dtb, x, tauInv, betaTauInv, lasso_max_iter):
     DtD:                D.T @ D
     Dtb:                D.T @ b
     x:                  initialization of x
+    f:                  objective function taking value x
     tauInv:             1 / tau
     betaTauInv:         1 / (beta * tau)
     lasso_max_iter:     maximum number of iterations
@@ -16,7 +17,8 @@ def FastIllinoisSolver(DtD, Dtb, x, tauInv, betaTauInv, lasso_max_iter):
     Output:
     x:  solution of lasso
     '''
-
+    x = x.reshape((x.shape[0],1))
+    Dtb = Dtb.reshape((x.shape[0],1))
     tol_apg = 1e-6 # tolerance
     t_prev = 1
     p = x
@@ -31,10 +33,12 @@ def FastIllinoisSolver(DtD, Dtb, x, tauInv, betaTauInv, lasso_max_iter):
         t_prev = t
         if np.linalg.norm(x_prev - x) < tol_apg * np.linalg.norm(x_prev):
             break
-        if np.dot(grad.T, x-x_prev) > 0:
+        #if np.dot(grad.T, x-x_prev) > 0:
+        if f(x) > f(x_prev):
             p = p_prev
             x = x_prev
             t = 1
             t_prev = t
             continue
+    x = np.squeeze(x)
     return x
