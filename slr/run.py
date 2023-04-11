9988e966d0a315cd81bcaf0e23248eed196ed026
Gypsophila <119010096@link.cuhk.edu.cn>
2023-04-11 08:44:47 -0400

    Update run.py

--- slr/run.py
+++ slr/run.py
@@ -1,8 +1,8 @@
-# from GenerateDataMatrix2 import GenerateDataMatrix
+from GenerateDataMatrix2 import GenerateDataMatrix
 
 ### switch to the first method of generating data
 ### Give each video a label
-from GenerateDataMatrix1 import GenerateDataMatrix
+# from GenerateDataMatrix1 import GenerateDataMatrix
 from CountVideosPerClass import CountVideosPerClass
 from GetClassIndex import GetClassIndex
 from ccSolveModel import ccSolveModel
@@ -14,8 +14,8 @@ import pandas as pd
 
 
 path = "../../SLR/stroke_data/data_new"
-# X, y = GenerateDataMatrix(path,30)
-X, y = GenerateDataMatrix(path)
+X, y = GenerateDataMatrix(path,30)
+# X, y = GenerateDataMatrix(path)
 
 ### Use second generation method
 #X, y = GenerateDataMatrix(path, 10)
@@ -33,8 +33,8 @@ test_x = np.zeros((1))
 train_y = []
 test_y = []
 begin = False
-train_sample_per_class = 2000
-test_sample_per_class = 10
+train_sample_per_class = 400
+test_sample_per_class = 20
 
 
 for i in range(4):
@@ -59,12 +59,12 @@ for i in range(4):
 
 
 dictionary = Normalize(train_x)
-# test_x = Normalize(test_x)
+test_x = Normalize(test_x)
 # dictionary = train_x
 
 global_max_iter=30
 lasso_max_iter=100
-alpha = 10
+alpha = 1
 # confussion_matrix=np.zeros((7,7))
 num_correct_classified=0
 num_experiments_run=0
@@ -73,17 +73,17 @@ num_class = 4
 
 summary = pd.DataFrame(columns=['Match', 'Real'])
 print("Generating")
-for i in range(2,num_class+1):
+for i in range(1,num_class):
     test_data = test_x[:,(i - 1) * test_sample_per_class:(i) * test_sample_per_class]
-    # for j in range(test_sample_per_class):
-    # test_x = test_data[:,j].reshape(-1,1)
-    matched_label, Xr, Lr = ccSolveModel(dictionary, train_y, test_data, num_class, global_max_iter, lasso_max_iter, alpha)
-    print('Label: Matched %d - Real %d \n'%(matched_label,i))
-    if (matched_label==i):
-        num_correct_classified += 1
-    summary.loc[num_experiments_run] = [matched_label,i]
-    num_experiments_run += 1
-    print('Partial Recognition Rate = %f \n'%(num_correct_classified / num_experiments_run))
+    for j in range(test_sample_per_class):
+        test_x = test_data[:,j].reshape(-1,1)
+        matched_label, Xr, Lr = ccSolveModel(dictionary, train_y, test_x, num_class, global_max_iter, lasso_max_iter, alpha)
+        print('Label: Matched %d - Real %d \n'%(matched_label,i))
+        if (matched_label==i):
+            num_correct_classified += 1
+        summary.loc[num_experiments_run] = [matched_label,i]
+        num_experiments_run += 1
+        print('Partial Recognition Rate = %f \n'%(num_correct_classified / num_experiments_run))
 summary.to_csv("summary.csv")
 
 # conf_mat = confusion_matrix(test_y, matched_pred)
