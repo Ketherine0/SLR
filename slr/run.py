c91e745cd953670927457d3e620fd1aaccda2be8
Gypsophila <119010096@link.cuhk.edu.cn>
2023-04-11 14:22:50 -0400

    Update run.py

--- slr/run.py
+++ slr/run.py
@@ -77,6 +77,7 @@ num_class = 4
 
 summary = pd.DataFrame(columns=['Match', 'Real'])
 print("Generating")
+a=0
 for i in range(2,num_class+1):
     test_data = test_x[:, (i - 1) * test_frame_per_sample*test_sample_per_class:(i) * test_frame_per_sample*test_sample_per_class]
     for j in range(test_sample_per_class):
@@ -84,6 +85,8 @@ for i in range(2,num_class+1):
         # for j in range(test_sample_per_class):
         test_frame = test_data[:,j*test_frame_per_sample:(j+1)*test_frame_per_sample]
         matched_label, Xr, Lr, error = ccSolveModel(dictionary, train_y, test_frame, num_class, global_max_iter, lasso_max_iter, alpha, lambdaG, delta)
+        a+= 1
+        print("Sample: ",a)
         print('Label: Matched %d - Real %d \n'%(matched_label,i))
         if (matched_label==i):
             num_correct_classified += 1
