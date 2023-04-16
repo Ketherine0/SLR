9a92a013b915cd9f8f77618a8ca02e271a6dbcaa
Gypsophila <119010096@link.cuhk.edu.cn>
2023-04-16 09:39:47 -0400

    Update run.py

--- slr/run.py
+++ slr/run.py
@@ -1,8 +1,8 @@
-from GenerateDataMatrix2 import GenerateDataMatrix
+# from GenerateDataMatrix2 import GenerateDataMatrix
 
 ### switch to the first method of generating data
 ### Give each video a label
-# from GenerateDataMatrix1 import GenerateDataMatrix
+from GenerateDataMatrix1 import GenerateDataMatrix
 from CountVideosPerClass import CountVideosPerClass
 from GetClassIndex import GetClassIndex
 from ccSolveModel import ccSolveModel
@@ -15,8 +15,8 @@ import matplotlib.pyplot as plt
 
 
 path = "../../SLR/stroke_data/data_new"
-X, y = GenerateDataMatrix(path,30)
-# X, y = GenerateDataMatrix(path)
+# X, y = GenerateDataMatrix(path,30)
+X, y = GenerateDataMatrix(path)
 
 ### Use second generation method
 #X, y = GenerateDataMatrix(path, 10)
@@ -80,7 +80,6 @@ print("Generating")
 idx=0
 for i in range(2,num_class+1):
     test_data = test_x[:, (i - 1) * test_frame_per_sample*test_sample_per_class:(i) * test_frame_per_sample*test_sample_per_class]
-    print(test_data.shape)
     for j in range(test_sample_per_class):
         test_frame = test_data[:,j*test_frame_per_sample:(j+1)*test_frame_per_sample]
         matched_label, Xr, Lr, error = ccSolveModel(dictionary, train_y, test_frame, num_class, global_max_iter, lasso_max_iter, alpha, lambdaG, delta)
@@ -92,10 +91,10 @@ for i in range(2,num_class+1):
         summary.loc[num_experiments_run] = [matched_label,i]
         num_experiments_run += 1
         print('Partial Recognition Rate = %f \n'%(num_correct_classified / num_experiments_run))
-        plt.plot(np.arange(len(error) - 1), np.log10(np.array(error[:-1] - error[-1])), 'b')
-        plt.xlabel("Number of iteration")
-        plt.ylabel("Error")
-        plt.show()
+        # plt.plot(np.arange(len(error) - 1), np.log10(np.array(error[:-1] - error[-1])), 'b')
+        # plt.xlabel("Number of iteration")
+        # plt.ylabel("Error")
+        # plt.show()
 
-summary.to_csv("summary.csv")
+summary.to_csv("summary1.csv")
 
