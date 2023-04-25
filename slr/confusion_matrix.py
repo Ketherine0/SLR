478260e110c795309025231c7f885b4677fa8750
ashlynzzzz <kxinzhao@umich.edu>
2023-04-25 19:29:38 -0400

    Delete confusion_matrix.py

--- slr/confusion_matrix.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.metrics import confusion_matrix
-
-summary = pd.read_csv('summary.csv').iloc[:,1:]
-y_pred = summary.iloc[:,0]
-y_true = summary.iloc[:,1]
-conf = confusion_matrix(y_true,y_pred)
-print(conf)
